02711cef7acfb9c00eededb83fef6da22e039687
jimmy-print <jimmy.wang123@outlook.sg>
2021-10-14 21:07:17 +0800

    partially add linear motion for angular.py

--- angular.py
+++ angular.py
@@ -3,6 +3,73 @@ import time
 import math
 from pygame.locals import *
 
+VERT = 'VERT'
+HORZ = 'HORZ'
+
+WHITE = 255, 255, 255
+BLACK = 0, 0, 0
+RED = 255, 0, 0
+GREEN = 0, 255, 0
+BLUE = 0, 0, 255
+
+class TwoAxisDirection:
+    def __init__(self, sign, XY):
+        self.sign = sign
+        assert XY == VERT or XY == HORZ
+        self.XY = XY
+
+class Force:
+    def __init__(self, magnitude, direction, name):
+        self.magnitude = magnitude  # newtons
+        self.direction = direction
+        self.name = name
+
+    def __repr__(self):
+        return f'F{self.name} {self.direction.sign} * {self.magnitude} N {self.direct}'
+
+
+mass = 1
+g_accel = 0.05
+Fw = Force(g_accel * mass, TwoAxisDirection(sign=-1, XY=VERT), name="weight")
+
+y_forces = []
+x_forces = []
+
+def draw_forces(y_forces__, x_forces__, x, y):
+    for force in y_forces__:
+        if force.name == "applied":
+            color = RED
+        else:
+            color = WHITE
+
+        s = font.render(f'F{force.name} {force.magnitude} N', True, WHITE)
+
+        if force.direction.sign == 1:
+            height = 100
+            pygame.draw.rect(display, color, (x, (D_HEIGHT - y) - height, 5, height))
+            display.blit(s, (x + 10, D_HEIGHT - y - height / 2))
+        elif force.direction.sign == -1:
+            height = 100
+            pygame.draw.rect(display, color, (x, D_HEIGHT - y, 5, height))
+            display.blit(s, (x + 10, D_HEIGHT - y + height / 2))
+
+    for force in x_forces__:
+        if force.name == "applied":
+            color = RED
+        else:
+            color = WHITE
+
+        s = font.render(f'F{force.name} {force.magnitude} N', True, WHITE)
+
+        if force.direction.sign == 1:
+            width = 100
+            pygame.draw.rect(display, color, (x + width / 10, (D_HEIGHT - y), width, 5))
+            display.blit(s, (x + width / 8, D_HEIGHT - y - width / 5))
+        elif force.direction.sign == -1:
+            width = 100
+            pygame.draw.rect(display, color, (x - width, (D_HEIGHT - y), width, 5))
+            display.blit(s, (x - s.get_rect().width - width / 8, D_HEIGHT - y - width / 5))
+
 c = {
     "mass": 1,
     "radius": 0,  # moment arm distance?
@@ -10,12 +77,12 @@ c = {
 
 p0 = {
     "mass": 1,
-    "radius": 300,
+    "radius": 100,
 }
 
 p1 = {
     "mass": 1,
-    "radius": -300,
+    "radius": -100,
 }
 
 line = c, p0, p1
@@ -57,6 +124,9 @@ line = c, p0, p1
 D_WIDTH, D_HEIGHT = 1280, 720
 display = pygame.display.set_mode((D_WIDTH, D_HEIGHT))
 
+pygame.font.init()
+font = pygame.font.Font("inconsolata.ttf", 15)
+
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 
@@ -97,7 +167,8 @@ def draw_point(p):
 # t = 40 000kgm * 0.01 rad / s^2
 # t = 400Nm??
 
-torque = 0.1  # newton meters
+#torque = 0.1  # newton meters
+torque = 0
 p0_ang = 0
 p0_ang_speed = 0
 
@@ -106,8 +177,14 @@ p1_ang_speed = 0
 
 fpsclock = pygame.time.Clock()
 fps_desired = 60
+
+t = 0
+
+linear_velocity = [2, 30]  # same for all particles
+
     
 while True:
+    t+=1
     display.fill(BLACK)
 
     for event in pygame.event.get():
@@ -121,6 +198,18 @@ while True:
     # N / kg = rad / s ^2
     # m / m = 1
     # ang. accel = 0.01 rad / s^2
+
+    y_forces.append(Fw)
+
+    draw_forces(y_forces__=y_forces, x_forces__=x_forces, x=p0['x'], y=p0['y'])
+    draw_forces(y_forces__=y_forces, x_forces__=x_forces, x=p1['x'], y=p1['y'])
+
+
+    if 0 < t < 2:
+        torque = 2
+    else:
+        torque = 0
+
     p0_moment_of_inertia = (p0['radius'] ** 2 * p0['mass'])
     #assert p0_moment_of_inertia == 40000
     p0_angaccel = torque / p0_moment_of_inertia
@@ -136,6 +225,9 @@ while True:
     p1_ang_speed += p1_angaccel
     p1_ang += p1_ang_speed
 
+    c['x'] = center_x
+    c['y'] = center_y  # TODO simplify this
+
     p0_dx = math.cos(p0_ang * 180 / math.pi) * p0['radius']
     p0['x'] = center_x + p0_dx
 
@@ -149,14 +241,18 @@ while True:
     p1['y'] = center_y + p1_dy
 
 
+    print(p0_angaccel, p0_ang_speed)
 
-    
+
+
     
     draw_point(c)
     draw_point(p0)
     draw_point(p1)
 
     pygame.draw.line(display, WHITE, (p0['x'], D_HEIGHT - p0['y']), (p1['x'], D_HEIGHT - p1['y']))
+
+    y_forces = []
 
     pygame.display.update()
     fpsclock.tick(fps_desired)
